694c8dafdd99c72c4f46c326536126b3c1be59b6
Anastasiya <nastagh96@mail.ru>
2022-07-26 00:39:48 +0200

    feat:add2

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -389,8 +389,9 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  // throw new Error('Not implemented');
+  return arr.filter((x) => x === item).length;
 }
 
 /**
